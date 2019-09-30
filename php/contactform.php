cfc5d856cecb565c4a33a831ab93ed3469eb8bd8
letitiat <letitiatyrrell2@gmail.com>
2019-09-30 10:45:47 +0100

    Added functionality to contact form. Changed copy.

---
+++ php/contactform.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $reason = $_POST['reason'];
+  $mailFrom = $_POST['mail'];
+
+  $mailTo = "letitia.tyrrell@hotmail.com";
+  $headers = "From: ".$mailFrom;
+  $txt = "You have recieved an email from ".$name.".\n\n".$message;
+
+  mail($mailTo, $subject, $txt, $headers);
+  header("Location: index.php?mailsent");
+
+}
+
+ ?>
